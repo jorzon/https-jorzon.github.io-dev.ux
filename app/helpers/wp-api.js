5bab83d33dbb75574680fe94839a5dc4725ed922
Jorzon Olesky Delgado Orillo - TCS <jorzondelgado@bcp.com.pe>
2022-04-19 21:40:04 -0500

    change domain

--- app/helpers/wp-api.js
+++ app/helpers/wp-api.js
@@ -1,5 +1,5 @@
-const NAME = 'https://jorzon.github.io/wux/',
-    DOMAIN = `https://jorzon.github.io/wux/`,
+const NAME = 'https://jorzon.github.io/wux',
+    DOMAIN = `https://jorzon.github.io/wux`,
     SITE = `${DOMAIN}`,
     CARDS = `${SITE}/json/cards.json`,
     DETAIL = `${SITE}/json/`
